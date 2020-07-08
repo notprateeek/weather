0b805c9f6e1ac415eea5863f0b929b193346ebcf
Prateek <notprateeek@gmail.com>
2020-07-09 01:40:51 +0530

    almost perfect

--- app.js
+++ app.js
@@ -1,55 +1,57 @@
 window.addEventListener('load', () => {
-  let long;
-  let lat;
-  let temperatureDescription = document.querySelector(
+  let lat, lon;
+  const temperatureDescription = document.querySelector(
     '.temperature-description'
   );
-  let temperatureDegree = document.querySelector('.temperature-degree');
-  let locationTimezone = document.querySelector('.location-timezone');
-  let temperatureSection = document.querySelector('.temperature');
+  const locationTimezone = document.querySelector('.location-timezone');
+  const temperatureDegree = document.querySelector('.temperature-degree');
+  const temperatureSection = document.querySelector('.temperature');
   const temperatureSpan = document.querySelector('.temperature span');
+  const weatherIcon = document.querySelector('.weather-icon');
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
-      long = position.coords.longitude;
+      lon = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}`;
+      const key = 'e343f2d0a39531a8b2ec7b84c41f89e9';
 
-      //problem starts here
-      console.log(position);
+      //use proxy if this does not work on live server
+      // const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
+
       fetch(api)
         .then((response) => {
           return response.json();
         })
         .then((data) => {
-          const { temperature, summary, icon } = data.currently;
-          // set DOM elements from API
-          temperatureDegree.textContent = temperature;
-          temperatureDescription.textContent = summary;
-          locationTimezone.textContent = data.timezone;
-          // formula
-          let celsius = (temperature - 32) * (5 / 9);
-          // set icon
-          setIcons(icon, document.querySelector('.icon'));
-          // change degree
+          const temp = Math.floor(data.main.temp - 273.15);
+          const { description, icon } = data.weather[0];
+          const location = data.name + ', ' + data.sys.country;
+
+          //set DOM elements from API
+          temperatureDegree.textContent = temp;
+          temperatureDescription.textContent = description;
+          locationTimezone.textContent = location;
+
+          //formula
+          let fahrenheit = (temp * 9) / 5 + 32;
+
+          //change temperature
           temperatureSection.addEventListener('click', () => {
-            if (temperatureSpan.textContent === 'F') {
-              temperatureSpan.textContent = 'C';
-              temperatureDegree.textContent = Math.floor(celsius);
-            } else {
+            if (temperatureSpan.textContent === 'C') {
               temperatureSpan.textContent = 'F';
-              temperatureDegree.textContent = temperature;
+              temperatureDegree.textContent = Math.floor(fahrenheit);
+            } else {
+              temperatureSpan.textContent = 'C';
+              temperatureDegree.textContent = temp;
             }
           });
+
+          //weather icon
+          weatherIcon.innerHTML = `<img src="icons/${icon}.png">`;
         });
     });
-  }
-
-  function setIcons(icon, iconID) {
-    const skycons = new Skycons({ color: 'white' });
-    const currentIcon = icon.replace(/-/g, '_').toUpperCase();
-    skycons.play();
-    return skycons.set(iconID, Skycons[currentIcon]);
   }
 });
